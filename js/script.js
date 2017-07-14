2b2cc62e69aeb287fe57fd1c670d30fa2b19f71c
Monko <s1me0n4o@abv.bg>
2017-07-14 15:32:08 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// start JS document
+
+
+// for (var i = 0; i <= 10; i++) {
+//     var selektor = "\"active-" + i + "\"spans:nth-child(n+" + i + ")\"";
+//     var skills = document.getElementsByClassName(selektor)
+//     for (var k = 0; k < skills.length; k++) {
+
+//         console.log(skills);
+//     }
+// }
+function on() {
+    document.getElementById("thumb-overlay").style.display = "block";
+}
+
+function off() {
+    document.getElementById("thumb-overlay").style.display = "none";
+}
